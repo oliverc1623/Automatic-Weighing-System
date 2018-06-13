4c8e6a4aeabe158e5b363b4e768760b9dab97bf0
oliverc1623 <oliverc1622@gmail.com>
2018-06-13 16:43:02 -0700

    added python script

--- weight_test/weight_test.ino
+++ weight_test/weight_test.ino
@@ -1,11 +1,11 @@
-#include <HX711.h>
+#include <HX711.h>cd
 
 HX711 scale(3,2);
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
   Serial.begin(9600);  
-  Serial.println("Press T/t to tare");
+  //Serial.println("Press T/t to tare");
   
   float fiveKgCf = -36650; //calibration factor for 5kg load cell
   float oneKgCF = -85000; //calibration factor for 1kg load cell
@@ -16,10 +16,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Weight: ");
-  Serial.print(scale.get_units(), 3);  //Up to 3 decimal points
+  //Serial.print("Weight: ");
+  Serial.println(scale.get_units(), 3);  //Up to 3 decimal points
   //Serial.print((scale.read_average(0)-scale.get_offset())/-85000,3);
-  Serial.println(" kg"); //Change this to kg and re-adjust the calibration factor if you follow lbs
+  //Serial.println(" kg"); //Change this to kg and re-adjust the calibration factor if you follow lbs
   
   if(Serial.available())
   {
