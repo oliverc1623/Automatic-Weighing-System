d021c11bad1a42aef849ef2f1e8591acb7616489
oliverc1623 <oliverc1622@gmail.com>
2018-07-26 12:52:55 -0700

    added comments to scale_program

--- scale_program/scale_program.ino
+++ scale_program/scale_program.ino
@@ -1,72 +1,58 @@
-#include <HX711.h>
+#include <HX711.h> // include Hx711 package to call functions on the load cell
 
-HX711 scale(3,2);
-
-byte tmp;
-float x;
-float test = 2.322;
+HX711 scale(3,2); // use digital pors 3 & 2;
+                  // use digital ports 30 & 20 if using arduini mini
+                  
+byte tmp; // placholder variable we use to check
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
-    // put your setup code here, to run once:
+   // put your setup code here, to run once:
   Serial.begin(9600);  
-  //Serial.println("Press T/t to tare");
-  
-  float fiveKgCf = -382760; //calibration factor for 5kg load cell old: 45350
+
+  /* These are the default calibration factor for each type of load cell
+     However, EVERY load cell will hace a DIFFERENT calibration factor
+     These are just rough estimates for each type of load cell 
+  */ 
+  float fiveKgCf = -38650; //calibration factor for 5kg load cell old: 45350
   float oneKgCF = -84450; //calibration factor for 1kg load cell
   float twentyKgCF = -128850; // 66650
 
   float calibrationFactor;
-  
-  scale.set_scale(oneKgCF);  //Calibration Factor obtained from first sketch
-  scale.tare();   
+
+  // Default calibration factor is the 5 kg value
+  scale.set_scale(fiveKgCf);  
+
+  // tare the scale at the start
+  scale.tare();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.print("Weight: ");
-  //Serial.println(scale.get_units());  //Up to 3 decimal points
-  //Serial.println(" kg"); //Change this to kg and re-adjust the calibration factor if you follow lbs
-  
-  float f;
 
-  float fiveKgCf = -38000; //calibration factor for 5kg load cell
-  float oneKgCF = -84450; //calibration factor for 1kg load cell
-  float twentyKgCF = -66650; // 66650
-
+  // check for uncoming bytes sent from the matlab program
   if(Serial.available())
   {
+    // set the temp equal to the matlab value we sent e.g. 'r', 't'...
     char temp = Serial.read();
-    //String temp = Serial.readString();
-    //Serial.println(temp);
-    
     if(temp == 't' || temp == 'T')
-      scale.tare();  //Reset the scale to zero  
+      scale.tare();  //Reset the scale to zero
       
-    if(temp == 'r' || temp == 'R')
-        //Serial.write(45);
-        Serial.println(scale.get_units(), 4);
-        // Serial.println(scale.get_units());
-//
-//    if(temp == 's' || temp == 'S')
-//        // Set the scaling factor for 1kg
-//        scale.set_scale(oneKgCF);
-//    if(temp == 'g' || temp == 'G')
-//        // Set the scaling factor for 5kg
-//        scale.set_scale(fiveKgCf);
-//    if(temp == 'k' || temp == 'K')
-//        // Set the scaling factor for 20kg
-//        scale.set_scale(twentyKgCF);
+    // Have the arduino send the weight to matlab program  
+    if(temp == 'r' || temp == 'R') 
+        /* 
+         * print weight in kg to units
+         * the second argument in println is the amount of decimal places to return 
+         */
+        Serial.println(scale.get_units(), 4); 
     if(temp == 's'){
+        // read the new cf value matlab wrote to arduino
         String cfString = Serial.readString();
-        //Serial.println(cfString);
+        // convert string to float value
         float newCF = cfString.toFloat();
+        // call the set_cale method to change the calibration factor
         scale.set_scale(newCF);  
     }
   }
 }
-//        char *c_Data = ( char* ) &f;
-//        Serial.println(sizeof(float));
-//        for( char c_Index = 0 ; c_Index < sizeof( float ) ; Serial.write( c_Data[ c_Index++ ] ) );
-      
+
